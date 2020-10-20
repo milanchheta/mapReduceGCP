973a28c9d7f04e1b479fc224f33da45f2ab3a1f0
milanchheta <milanchheta@gmail.com>
2020-10-20 17:10:01 -0400

    testEdit

--- Master/testMaster.py
+++ Master/testMaster.py
@@ -8,6 +8,6 @@ portNumber = parser.get('address', 'port')
 intm = xmlrpc.client.ServerProxy(('http://' + ipAddress + ':' + portNumber),
                                  allow_none=True)
 uniqueId = intm.init_cluster(3, 3)
-res2 = intm.run_mapred(uniqueId, "./Data/test.txt", "InvertedIndexMapper",
-                       "InvertedIndexReducer", "outputPath.txt")
+# res2 = intm.run_mapred(uniqueId, "./Data/test.txt", "InvertedIndexMapper",
+#                        "InvertedIndexReducer", "outputPath.txt")
 print(uniqueId)
