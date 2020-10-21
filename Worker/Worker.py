2b05cac34c5e5823c9b2eee683158aa04c156b68
milanchheta <milanchheta@gmail.com>
2020-10-21 04:12:22 -0400

    commit-3

--- Worker/Worker.py
+++ Worker/Worker.py
@@ -1,13 +1,14 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 import xmlrpc.client
-
+import logging
 import json
 from configparser import ConfigParser
 
 
 ##InvertedIndex mapper function
 def InvertedIndexMapper(data, filename):
+    logger.info("called WordCountMapper")
     data = data.lower()
     dataList = ''.join((c if c.isalpha() else ' ') for c in data).split()
     resultData = []
@@ -18,6 +19,8 @@ def InvertedIndexMapper(data, filename):
 
 ##WordCount mapper function
 def WordCountMapper(data, filename):
+    logger.info("called WordCountMapper")
+
     data = data.lower()
     dataList = ''.join((c if c.isalpha() else ' ') for c in data).split()
     resultData = []
@@ -28,6 +31,8 @@ def WordCountMapper(data, filename):
 
 ##InvertedIndex reducer function
 def InvertedIndexReducer(data):
+    logger.info("called InvertedIndexReducer")
+
     resultDict = {}
     for word in data:
         for entry in data[word]:
@@ -43,6 +48,7 @@ def InvertedIndexReducer(data):
 
 ##WordCount reducer function
 def WordCountReducer(data):
+    logger.info("called wordcount reducer")
     resultDict = {}
     for word in data:
         resultDict[word] = len(data[word])
@@ -51,20 +57,26 @@ def WordCountReducer(data):
 
 ##store data to key value store
 def storeOutputData(dataStoreObj, result, path):
+    logger.info("storing datastore object")
     data = 'set-data' + ' ' + path + '\n' + json.dumps(result) + '\n'
     dataStoreObj.DataStore(data)
+    logger.info("stored datastore object")
     return
 
 
 ##get data from key value store
 def getInputData(file, dataStoreObj):
+    logger.info("fetching datastore object")
     responseMessage = 'get-data' + '\n' + file + '\n'
     jsonData = dataStoreObj.DataStore(responseMessage)
     jsonData = json.loads(jsonData)
+    logger.info("fetched datastore object")
+
     return jsonData
 
 
 def worker(uniqueId, worker, file, passedFunction, caller, kvIP, taskNumber=0):
+    logger.info("called worker")
     ##function map of available applications
     functionMap = {
         "mapper": {
@@ -76,6 +88,8 @@ def worker(uniqueId, worker, file, passedFunction, caller, kvIP, taskNumber=0):
             "WordCountReducer": WordCountReducer
         }
     }
+
+    logger.info("creating datastore object")
     ##connect to the keyvaluestore
     dataStoreObj = xmlrpc.client.ServerProxy(
         'http://' + kvIP + ':' + parser.get('address', 'keyValuePort'),
@@ -84,6 +98,7 @@ def worker(uniqueId, worker, file, passedFunction, caller, kvIP, taskNumber=0):
     result = getInputData(file, dataStoreObj)
 
     if caller == "mapper":
+        logger.info("called by mapper")
         for file in result:
             filename = file
             res = result[filename]
@@ -91,15 +106,30 @@ def worker(uniqueId, worker, file, passedFunction, caller, kvIP, taskNumber=0):
         path = "Data/" + uniqueId + "/mapperOutput/output" + str(worker) + str(
             taskNumber) + ".json"
     if caller == "reducer":
+        logger.info("called by reducer")
         resultData = functionMap[caller][passedFunction](result)
         path = "Data/" + uniqueId + "/reducerOutput/output" + str(
             worker) + ".json"
 
+    logger.info("storing mapper oiutput to kv store")
+
     ##Send data to keyvalue store
     storeOutputData(dataStoreObj, resultData, path)
 
+    logger.info("mapper task done")
+
 
 if __name__ == '__main__':
+    logger = logging.getLogger('worker-node')
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler('worker.log')
+    fh.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
     parser = ConfigParser()
     parser.read('config.ini')
 
@@ -117,7 +147,7 @@ if __name__ == '__main__':
 
     #run the rpc server
     try:
-        print('Worker running')
+        logger.info('Worker running')
         server.serve_forever()
     except Exception:
-        print('Error while running the server')
+        logger.info('Error while running the server')
