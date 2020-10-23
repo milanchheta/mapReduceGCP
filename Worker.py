9fe7050d60afb2c060ebf13ccacd2deb33e3f245
milanchheta <milanchheta@gmail.com>
2020-10-23 05:01:49 -0400

    commit-10

--- Worker.py
+++ Worker.py
@@ -4,57 +4,12 @@ import xmlrpc.client
 import logging
 import json
 from configparser import ConfigParser
-import importlib.util
-import marshal
+import os, stat
+
 from mappers.InvertedIndexMapper import InvertedIndexMapper
 from mappers.WordCountMapper import WordCountMapper
 from reducers.InvertedIndexReducer import InvertedIndexReducer
 from reducers.WordCountReducer import WordCountReducer
-import os, stat
-# ##InvertedIndex mapper function
-# def InvertedIndexMapper(data, filename):
-#     logger.info("called WordCountMapper")
-#     data = data.lower()
-#     dataList = ''.join((c if c.isalpha() else ' ') for c in data).split()
-#     resultData = []
-#     for word in dataList:
-#         resultData.append((word, filename))
-#     return resultData
-
-# ##WordCount mapper function
-# def WordCountMapper(data, filename):
-#     logger.info("called WordCountMapper")
-
-#     data = data.lower()
-#     dataList = ''.join((c if c.isalpha() else ' ') for c in data).split()
-#     resultData = []
-#     for word in dataList:
-#         resultData.append((word, 1))
-#     return resultData
-
-# ##InvertedIndex reducer function
-# def InvertedIndexReducer(data):
-#     logger.info("called InvertedIndexReducer")
-
-#     resultDict = {}
-#     for word in data:
-#         for entry in data[word]:
-#             if word in resultDict:
-#                 if entry[1] in resultDict[word]:
-#                     resultDict[word][entry[1]] += 1
-#                 else:
-#                     resultDict[word][entry[1]] = 1
-#             else:
-#                 resultDict[word] = {entry[1]: 1}
-#     return resultDict
-
-# ##WordCount reducer function
-# def WordCountReducer(data):
-#     logger.info("called wordcount reducer")
-#     resultDict = {}
-#     for word in data:
-#         resultDict[word] = len(data[word])
-#     return resultDict
 
 
 def isWorkerConnected():
@@ -63,22 +18,27 @@ def isWorkerConnected():
 
 ##store data to key value store
 def storeOutputData(dataStoreObj, result, path):
-    logger.info("storing datastore object")
-    data = 'set-data' + ' ' + path + '\n' + json.dumps(result) + '\n'
-    dataStoreObj.DataStore(data)
-    logger.info("stored datastore object")
-    return
+    try:
+        logger.info("storing datastore object")
+        data = 'set-data' + ' ' + path + '\n' + json.dumps(result) + '\n'
+        dataStoreObj.DataStore(data)
+        logger.info("stored datastore object")
+        return
+    except Exception as e:
+        raise e
 
 
 ##get data from key value store
 def getInputData(file, dataStoreObj):
-    logger.info("fetching datastore object")
-    responseMessage = 'get-data' + '\n' + file + '\n'
-    jsonData = dataStoreObj.DataStore(responseMessage)
-    jsonData = json.loads(jsonData)
-    logger.info("fetched datastore object")
-
-    return jsonData
+    try:
+        logger.info("fetching datastore object")
+        responseMessage = 'get-data' + '\n' + file + '\n'
+        jsonData = dataStoreObj.DataStore(responseMessage)
+        jsonData = json.loads(jsonData)
+        logger.info("fetched datastore object")
+        return jsonData
+    except Exception as e:
+        raise e
 
 
 def worker(uniqueId, worker, file, passedFunction, caller, kvIP, taskNumber=0):
@@ -99,36 +59,38 @@ def worker(uniqueId, worker, file, passedFunction, caller, kvIP, taskNumber=0):
             "WordCountReducer.py": WordCountReducer
         }
     }
+    try:
+        logger.info("creating datastore object")
+        ##connect to the keyvaluestore
+        dataStoreObj = xmlrpc.client.ServerProxy(
+            'http://' + kvIP + ':' + parser.get('address', 'keyValuePort'),
+            allow_none=True)
 
-    logger.info("creating datastore object")
-    ##connect to the keyvaluestore
-    dataStoreObj = xmlrpc.client.ServerProxy(
-        'http://' + kvIP + ':' + parser.get('address', 'keyValuePort'),
-        allow_none=True)
+        result = getInputData(file, dataStoreObj)
+        # operateFunc = marshal.loads(passedFunction)
 
-    result = getInputData(file, dataStoreObj)
-    # operateFunc = marshal.loads(passedFunction)
+        if caller == "mapper":
+            for file in result:
+                filename = file
+                res = result[filename]
+            resultData = functionMap[caller][passedFunction](res, filename)
+            # resultData = operateFunc(res, filename)
+            path = "Data/" + uniqueId + "/mapperOutput/output" + str(
+                worker) + str(taskNumber) + ".json"
+        if caller == "reducer":
+            resultData = functionMap[caller][passedFunction](result)
+            # resultData = operateFunc(res, filename)
+            path = "Data/" + uniqueId + "/reducerOutput/output" + str(
+                worker) + ".json"
 
-    if caller == "mapper":
-        for file in result:
-            filename = file
-            res = result[filename]
-        resultData = functionMap[caller][passedFunction](res, filename)
-        # resultData = operateFunc(res, filename)
-        path = "Data/" + uniqueId + "/mapperOutput/output" + str(worker) + str(
-            taskNumber) + ".json"
-    if caller == "reducer":
-        resultData = functionMap[caller][passedFunction](result)
-        # resultData = operateFunc(res, filename)
-        path = "Data/" + uniqueId + "/reducerOutput/output" + str(
-            worker) + ".json"
+        logger.info("storing mapper oiutput to kv store %s", path)
 
-    logger.info("storing mapper oiutput to kv store %s", path)
+        ##Send data to keyvalue store
+        storeOutputData(dataStoreObj, resultData, path)
 
-    ##Send data to keyvalue store
-    storeOutputData(dataStoreObj, resultData, path)
-
-    logger.info("mapper task done")
+        logger.info("mapper task done")
+    except Exception as e:
+        raise e
 
 
 if __name__ == '__main__':
